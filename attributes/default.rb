3f6f25a62cdd54bfd7e8ac393fc83ac103edbaf2
Ruben Koster <ruben.koster@innovationfactory.eu>
2012-10-19 16:44:49 +0200

    Fix FC001

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ include_attribute "cloudfoundry-common"
 # Port, user and password are set in cloudfoundry_common.
 
 # Where to write Nats's pid.
-default[:nats_server][:pid_file]   = File.join(node[:cloudfoundry_common][:pid_dir], "nats-server.pid")
+default['nats_server']['pid_file']   = File.join(node['cloudfoundry_common']['pid_dir'], "nats-server.pid")
 
 # Where to write Nats's logs.
-default[:nats_server][:log_file]   = File.join(node[:cloudfoundry_common][:log_dir], "nats-server.log")
+default['nats_server']['log_file']   = File.join(node['cloudfoundry_common']['log_dir'], "nats-server.log")
